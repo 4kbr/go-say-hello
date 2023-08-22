5cbb4f14fb64e20058d6e847a33849a56d709cae
4kbr <igyutubfesbuk@gmail.com>
2023-08-22 15:16:18 +0700

    add parameter name in SayHello

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Ohayou sekai good morning world"
+func SayHello(name string) string {
+	return "Ohayou sekai good morning world, " + name
 }
